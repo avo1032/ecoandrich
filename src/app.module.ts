d8e25ee54e94206e196482e1676fd9fda38571fb
swan1032@naver.com <swan1032@naver.com>
2023-03-28 12:24:14 +0900

    [FEAT] 데이터베이스 연결 구현

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EmployeesModule } from './employees/employees.module';
 
 @Module({
-  imports: [EmployeesModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: 3306,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [process.env.DB_ENTITIES],
+      synchronize: false,
+      timezone: 'local',
+      keepConnectionAlive: true,
+    }),
+    EmployeesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

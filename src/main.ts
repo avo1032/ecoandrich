d4894c3ed807981d22c1160d12d16957631e0155
swan1032@naver.com <swan1032@naver.com>
2023-03-30 10:18:48 +0900

    [FEAT] Timeout, SuccessInterceptor 구현

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SuccessInterceptor } from './common/interceptors/success.interceptor';
+import { TimeoutInterceptor } from './common/interceptors/timeout.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new TimeoutInterceptor(), new SuccessInterceptor());
   await app.listen(3000);
 }
 bootstrap();
